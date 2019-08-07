1377418bdb009caafeec0dd9831bb04f80f739a1
Anton Ilzheev <a@lovedesign.ru>
2019-08-07 14:35:45 +0400

    init

---
+++ FactomAPI.php
@@ -0,0 +1,392 @@
+<?php
+/**
+ * PHP SDK for Factom PRO
+ * @author Factom PRO <team@factom.pro>
+ */
+class FactomAPI
+{
+    const ENDPOINT = 'https://api.factom.pro';
+    const VERSION = 'v1';
+    protected
+        $api_key = null,
+        $response = null;
+        
+    /**
+    * API declaration
+    */
+    public function __construct($api_key)
+    {
+        $this->endpoint = self::VERSION."/".self::VERSION;
+	    $this->api_key = $api_key;
+    }
+    /**
+    * Create chain
+    * $extIds, array — One or many external ids identifying new chain. Should be sent as array of strings.
+    * $content, string — (optional) The content of the first entry of the chain.
+    */
+    public function createChain($extIds, $content="")
+    {	
+	    $chain["extIds"] = $this->helper_base64_encode($extIds);
+        $chain["content"] = $this->helper_base64_encode($content);
+        $res = $this->make_request('/chains', $chain, 'POST');
+        if (isset($res["result"])) {
+            $res["result"]["extIds"] = $this->helper_base64_decode($res["result"]["extIds"]);
+        }
+        return $res;
+    }
+
+    /**
+    * Create entry into chain
+    * $chainId, string — Chain ID of the Factom chain, where to add new entry.
+    * $extIds, array — (optional) One or many external ids identifying new chain. Should be sent as array of strings.
+    * $content, string — (optional) The content of the new entry.
+    */
+    public function createEntry($chainId, $extIds=NULL, $content="")
+    {
+        $entry["chainId"] = $chainId;
+        $entry["content"] = $this->helper_base64_encode($content);
+        if (isset($extIds)) {
+            $entry["extIds"] = $this->helper_base64_encode($extIds);	    
+            }
+            $res = $this->make_request('/entries', $entry, 'POST');
+            if (isset($res["result"])) {
+                if (isset($res["result"]["content"])) {
+                    $res["result"]["content"] = $this->helper_base64_decode($res["result"]["content"]);
+                }
+                if (isset($res["result"]["extIds"])) {
+                    $res["result"]["extIds"] = $this->helper_base64_decode($res["result"]["extIds"]);
+                }
+            }
+        return $res;
+    }
+
+    /**
+    * Search user's chains by extIds
+    * $extIds, array — One or many external IDs, that used for search. Should be sent as array of strings.
+    */
+    public function searchChains($extIds, $start=0, $limit=0, $status=NULL, $sort=NULL)
+    {
+        $queryParams = "";
+        if (($start > 0) || ($limit>0) || $sort) {
+            $queryParams .= "?";
+            if ($start > 0) {
+                $queryParams .= "start=";
+                $queryParams .= $start;
+                $queryParams .= "&";
+            }
+            if ($limit > 0) {
+                $queryParams .= "limit=";
+                $queryParams .= $limit;
+                $queryParams .= "&";
+            }
+            if ($status) {
+                $queryParams .= "status=";
+                $queryParams .= $status;
+                $queryParams .= "&";
+            }
+            if ($sort) {
+                $queryParams .= "sort=";
+                $queryParams .= $sort;
+                $queryParams .= "&";
+            }
+        }
+        $chain["extIds"] = $this->helper_base64_encode($extIds);
+        $res = $this->make_request('/chains/search'.$queryParams, $chain, 'POST');
+        if (isset($res["result"])) {
+            if (sizeof($res["result"])>0) {
+                foreach ($res["result"] as &$i) {
+                    $i["extIds"] = $this->helper_base64_decode($i["extIds"]);
+                }
+            }
+        }
+	    return $res;
+    }
+
+    /**
+    * Search chain entries
+    * $extIds, array — One or many external IDs, that used for search. Should be sent as array of strings.
+    */
+    public function searchChainEntries($chainId, $extIds, $start=0, $limit=0, $status=NULL, $sort=NULL)
+    {
+        $queryParams = "";
+        if (($start > 0) || ($limit>0) || $sort) {
+            $queryParams .= "?";
+            if ($start > 0) {
+                $queryParams .= "start=";
+                $queryParams .= $start;
+                $queryParams .= "&";
+            }
+            if ($limit > 0) {
+                $queryParams .= "limit=";
+                $queryParams .= $limit;
+                $queryParams .= "&";
+            }
+            if ($status) {
+                $queryParams .= "status=";
+                $queryParams .= $status;
+                $queryParams .= "&";
+            }
+            if ($sort) {
+                $queryParams .= "sort=";
+                $queryParams .= $sort;
+                $queryParams .= "&";
+            }
+        }
+        $entry["chainId"] = $chainId;
+        $entry["extIds"] = $this->helper_base64_encode($extIds);
+        $res = $this->make_request('/chains/'.$chainId.'/entries/search'.$queryParams, $entry, 'POST');
+        if (isset($res["result"])) {
+            if (sizeof($res["result"])>0) {
+                foreach ($res["result"] as &$i) {
+                    if (isset($i["content"])) {
+                        $i["content"] = $this->helper_base64_decode($i["content"]);
+                    }
+                    if (isset($i["extIds"])) {
+                        $i["extIds"] = $this->helper_base64_decode($i["extIds"]);
+                    }
+                }
+            }
+        }
+	    return $res;
+    }
+
+    /**
+    * Get user's chains
+    */
+    public function getChains($start=0, $limit=0, $status=NULL, $sort=NULL)
+    {	    
+        $queryParams = "";
+        if (($start > 0) || ($limit>0) || $sort) {
+            $queryParams .= "?";
+            if ($start > 0) {
+                $queryParams .= "start=";
+                $queryParams .= $start;
+                $queryParams .= "&";
+            }
+            if ($limit > 0) {
+                $queryParams .= "limit=";
+                $queryParams .= $limit;
+                $queryParams .= "&";
+            }
+            if ($status) {
+                $queryParams .= "status=";
+                $queryParams .= $status;
+                $queryParams .= "&";
+            }
+            if ($sort) {
+                $queryParams .= "sort=";
+                $queryParams .= $sort;
+                $queryParams .= "&";
+            }
+        }
+        $res = $this->make_request('/chains'.$queryParams);
+        if (isset($res["result"])) {
+            if (sizeof($res["result"])>0) {
+                foreach ($res["result"] as &$i) {
+                    $i["extIds"] = $this->helper_base64_decode($i["extIds"]);
+                }
+            }
+        }
+        return $res;
+    }
+
+    /**
+    * Get chain
+    * $chainId, string — Chain ID of the Factom chain.
+    */
+    public function getChain($chainId)
+    {	    
+        $res = $this->make_request('/chains/'.$chainId);
+        if (isset($res["result"])) {
+            $res["result"]["extIds"] = $this->helper_base64_decode($res["result"]["extIds"]);
+        }
+        return $res;
+    }
+
+    /**
+    * Get chain entries
+    * $chainId, string — Chain ID of the Factom chain.
+    */
+    public function getChainEntries($chainId, $start=0, $limit=0, $status=NULL, $sort=NULL)
+    {	    
+        $queryParams = "";
+        if (($start > 0) || ($limit>0) || $sort) {
+            $queryParams .= "?";
+            if ($start > 0) {
+                $queryParams .= "start=";
+                $queryParams .= $start;
+                $queryParams .= "&";
+            }
+            if ($limit > 0) {
+                $queryParams .= "limit=";
+                $queryParams .= $limit;
+                $queryParams .= "&";
+            }
+            if ($status) {
+                $queryParams .= "status=";
+                $queryParams .= $status;
+                $queryParams .= "&";
+            }
+            if ($sort) {
+                $queryParams .= "sort=";
+                $queryParams .= $sort;
+                $queryParams .= "&";
+            }
+        }
+        $res = $this->make_request('/chains/'.$chainId.'/entries'.$queryParams);
+        if (isset($res["result"])) {
+            if (sizeof($res["result"])>0) {
+                foreach ($res["result"] as &$i) {
+                    if (isset($i["content"])) {
+                        $i["content"] = $this->helper_base64_decode($i["content"]);
+                    }
+                    if (isset($i["extIds"])) {
+                        $i["extIds"] = $this->helper_base64_decode($i["extIds"]);
+                    }
+                }
+            }
+        }
+        return $res;
+    }
+
+    /**
+    * Get entry
+    * $entryHash, string — EntryHash of the Factom entry
+    */
+    public function getEntry($entryHash)
+    {	    
+        $res = $this->make_request('/entries/'.$entryHash);
+        if (isset($res["result"])) {
+            if (isset($res["result"]["content"])) {
+                $res["result"]["content"] = $this->helper_base64_decode($res["result"]["content"]);
+            }
+            if (isset($res["result"]["extIds"])) {
+                $res["result"]["extIds"] = $this->helper_base64_decode($res["result"]["extIds"]);
+            }
+        }
+        return $res;
+    }
+
+    /**
+    * Get chain first entry
+    * $chainId, string — Chain ID of the Factom chain.
+    */
+    public function getChainFirstEntry($chainId)
+    {	    
+        $res = $this->make_request('/chains/'.$chainId.'/entries/first');
+        if (isset($res["result"])) {
+            if (isset($res["result"]["content"])) {
+                $res["result"]["content"] = $this->helper_base64_decode($res["result"]["content"]);
+            }
+            if (isset($res["result"]["extIds"])) {
+                $res["result"]["extIds"] = $this->helper_base64_decode($res["result"]["extIds"]);
+            }
+        }
+        return $res;
+    }
+
+
+    /**
+    * Get chain last entry
+    * $chainId, string — Chain ID of the Factom chain.
+    */
+    public function getChainLastEntry($chainId)
+    {	    
+        $res = $this->make_request('/chains/'.$chainId.'/entries/last');
+        if (isset($res["result"])) {
+            if (isset($res["result"]["content"])) {
+                $res["result"]["content"] = $this->helper_base64_decode($res["result"]["content"]);
+            }
+            if (isset($res["result"]["extIds"])) {
+                $res["result"]["extIds"] = $this->helper_base64_decode($res["result"]["extIds"]);
+            }
+        }
+        return $res;
+    }
+
+    /**
+    * Get API user
+    */
+    public function getUser()
+    {	    
+        return $this->make_request('/user');
+    }
+
+    /**
+    * Get API version
+    */
+    public function getAPIInfo()
+    {	    
+        return $this->make_request('/');
+    }
+	
+    /**
+    * Make the request to Factom PRO API
+    */
+    protected function make_request($path, $data = NULL, $type = "GET")
+    {
+        $data = json_encode($data);
+        
+        if (function_exists('curl_init')) {
+            $ch = curl_init($this->endpoint.$path);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $type);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+            if ($data) {
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            }
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Authorization: Bearer ' . $this->api_key,                                                                          
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data))
+            );
+            $response = curl_exec($ch);
+            curl_close($ch);
+        }
+	    else {
+		    throw new Exception("CURL PHP extension required for this script");
+	    }
+        
+        return json_decode($response, true);
+    }
+
+    /**
+     * Helper base64_encode
+     */
+    protected function helper_base64_encode($input)
+    {
+
+        if (is_array($input)) {
+            foreach ($input as &$i) {
+                $i = base64_encode($i);
+            }
+        } else {
+            $input = base64_encode($input);
+        }
+
+        return $input;
+
+    }
+
+    /**
+     * Helper base64_decode
+     */
+    protected function helper_base64_decode($input)
+    {
+
+        if (is_array($input)) {
+            foreach ($input as &$i) {
+                $i = base64_decode($i);
+            }
+        } else {
+            $input = base64_decode($input);
+        }
+        
+        return $input;
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,139 @@
+# PHP client for Factom PRO API
+This is a PHP client for <a href="https://factom.pro" target="_blank">Factom PRO</a> API.
+
+## Usage
+
+### 1. Require library
+```php
+require_once("FactomAPI.php");
+```
+
+### 2. Initialize client
+```php
+$api_key = "YOUR_API_KEY";
+$factom = new FactomAPI($api_key);
+```
+
+### 3. Use client to work with Factom PRO API
+1. Get API info
+```php
+// Get API version
+$chains = $factom->getAPIInfo();
+```
+
+2. Get user info
+```php
+// Get user info
+$user = $factom->getUser();
+```
+
+3. Create a chain
+```php
+// Creates chain on the Factom blockchain
+$extIds[0] = "My new chain";
+$extIds[1] = "Second ExtID";
+$content = "Content of the first entry"; // optional
+$chain = $factom->createChain($extIds, $content);
+```
+
+4. Get chains
+```php
+// Get all user’s chains
+$chains = $factom->getChains();
+
+// Get user's chains from 41th to 60th
+$chains = $factom->getChains(40, 20);
+
+// Get user's chains with status "queue"
+// start=0, limit=0 — use defaults pagination params
+// status="queue" — filter chains by status "queue" (also "processing" | "completed")
+$chains = $factom->getChains(0, 0, "queue");
+
+// Get user's chains in reverse sorting (from oldest to newest)
+// start=0, limit=0 — use defaults pagination params
+// status=NULL — not filter by status
+// sort="asc" — sort results by createdAt ASC ("desc" is default sorting)
+$chains = $factom->getChains(0, 0, NULL, "asc");
+
+// Combine all filters and params
+// start=40, limit=20, status="queue", sort="asc"
+$chains = $factom->getChains(40, 20, "queue", "asc");
+```
+
+5. Get chain
+```php
+// Get Factom chain by Chain ID
+$chainId = "fb5ad150761da70e090cb2582445681e4c13107ca863f9037eaa2947cf7d225c";
+$chain = $factom->getChain($chainId);
+```
+
+6. Get chain entries
+```php
+// Get entries of Factom chain
+$chainId = "fb5ad150761da70e090cb2582445681e4c13107ca863f9037eaa2947cf7d225c";
+$entries = $factom->getChainEntries($chainId);
+
+// Filters and params may be applied to results
+// Example: start=40, limit=20, status="queue", sort="asc"
+$entries = $factom->getChainEntries($chainId, 40, 20, "queue", "asc");
+```
+
+7. Get first/last entry of the chain
+```php
+$chainId = "fb5ad150761da70e090cb2582445681e4c13107ca863f9037eaa2947cf7d225c";
+
+// Get first entry of Factom chain
+$firstEntry = $factom->getChainFirstEntry($chainId);
+
+// Get last entry of Factom chain
+$firstEntry = $factom->getChainLastEntry($chainId);
+```
+
+8. Search user's chains by external ids
+```php
+// Search for chains with tag "Forum Thread"
+$extIds[0] = "Forum Thread";
+$chains = $factom->searchChains($extIds);
+
+// Search for entries with 2 tags simultaneously 
+$extIds[1] = "v1.0.0";
+$chains2 = $factom->searchChains($extIds);
+
+// Filters and params may be applied to results
+// Example: start=40, limit=20, status="completed", sort="asc"
+$chains = $factom->searchChains($extIds, 40, 20, "completed", "asc");
+```
+
+9. Search chain entries by external ids
+```php
+// Search entries into Factom chain by external id(s)
+$chainId = "fb5ad150761da70e090cb2582445681e4c13107ca863f9037eaa2947cf7d225c";
+// Search for entries with tag "Forum Post"
+$extIds[0] = "Forum Post";
+$entries = $factom->searchChainEntries($chainId, $extIds);
+
+// Search for entries with 2 tags simultaneously 
+$extIds[1] = "v1.0.0";
+$entries2 = $factom->searchChainEntries($chainId, $extIds);
+
+// Filters and params may be applied to results
+// Example: start=40, limit=20, status="processing", sort="asc"
+$entries = $factom->searchChainEntries($chainId, $extIds, 40, 20, "processing", "asc");
+```
+
+10. Create an entry
+```php
+// Create entry in the Factom chain
+$chainId = "fb5ad150761da70e090cb2582445681e4c13107ca863f9037eaa2947cf7d225c";
+$extIds[0] = "My new entry";
+$extIds[1] = "Second ExtID";
+$content = "Content of the new entry";
+$entry = $factom->createEntry($chainId, $extIds, $content);
+```
+
+11. Get entry
+```php
+// Get Factom entry by EntryHash
+$entryHash = "dc2160b99b5f46f156e54bdebc81aef3243884b68b2c0c05e4741910738273f2";
+$entry = $factom->getEntry($entryHash);
+```
